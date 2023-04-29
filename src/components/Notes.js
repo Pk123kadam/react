7a6205832498ef8688c297b078788e21d5b137f5
pkadam5 <kadamprathaesh@gmail.com>
2023-04-29 11:52:48 +0530

    changes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,7 +4,7 @@ function Notes({ data, index, btn, up }) {
         <div className="d-flex flex-column gap-4 mt-5 bg-dark" style={{ width: "50%", margin: "5px auto 0px auto", color: "white", borderRadius: "20px", padding: "80px" }}>
             {/* <h1>{data.title}</h1> */}
 
-            <li className="  bg-primary p-3 rounded-pill m-auto" style={{ color: "white", fontSize: "20px", width: "40%" }}>{data.title}</li>
+            <li className="  bg-primary p-3 rounded-pill m-auto" style={{ color: "white", fontSize: "22px", width: "40%" }}>{data.title}</li>
 
             <p style={{ width: "100%", whiteSpace: "normal", wordBreak: "break-all" }}>{data.content}</p>
             <div>
